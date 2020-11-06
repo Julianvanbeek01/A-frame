586de28979974d1871a23199f5f037284a37026d
Julian van Beek <dev.beek@gmail.com>
2020-11-06 21:15:04 +0100

    Update main.js

--- 05Interaction/scripts/main.js
+++ 05Interaction/scripts/main.js
@@ -13,6 +13,12 @@ setInterval(spin, 16);
 
 
  myOtherBox.addEventListener('mouseenter', function(){
- 	rotationSpeed = 0.001;
+ 	rotationSpeed = 0.01;
  	console.log('mouse enter');
  });
+
+ myOtherBox.addEventListener('mouseleave', function(){
+ 	rotationSpeed = 0.001;
+ 	console.log('mouse leave');
+ });
+
